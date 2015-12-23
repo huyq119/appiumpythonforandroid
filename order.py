2f079057d811949fa53897e6af1b1074e44e472b
huyq119 <yuqi.hu@gmail.com>
2015-12-23 17:43:58 +0800

    success in searching

--- order.py
+++ order.py
@@ -1,3 +1,5 @@
+#coding:utf-8
+#coding:gb2312
 import unittest
 import time
 from appium import webdriver
@@ -9,34 +11,30 @@ desired_caps['appPackage']='com.izp.f2c'
 desired_caps['appActivity']='activity.HomeActivity' #config the cap
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-driver.find_element_by_id('com.izp.f2c:id/home_rb_discover').click() # use uiautomatorviewer to find resource id
-driver.find_element_by_id('com.izp.f2c:id/home_rb_mine').click()
-driver.find_element_by_id('com.izp.f2c:id/nologin').click()
-driver.find_element_by_id('com.izp.f2c:id/login_inputusername').click()
-driver.press_keycode(51)
-driver.press_keycode(43)
-driver.press_keycode(40)
-driver.press_keycode(34)
-driver.press_keycode(36)
-driver.press_keycode(49)
+# driver.find_element_by_id('com.izp.f2c:id/home_rb_discover').click() # use uiautomatorviewer to find resource id
+# driver.find_element_by_id('com.izp.f2c:id/home_rb_mine').click()
+# driver.find_element_by_id('com.izp.f2c:id/nologin').click()
+# driver.find_element_by_id('com.izp.f2c:id/login_inputusername').click()
+# driver.press_keycode(51)
+# driver.press_keycode(43)
+# driver.press_keycode(40)
+# driver.press_keycode(34)
+# driver.press_keycode(36)
+# driver.press_keycode(49)
 # input username 'wolfhu' please confirm your choice
-driver.find_element_by_id('com.izp.f2c:id/login_inputpassword').click()
-i=1
-while i<=6:
-    driver.press_keycode(8)
-    i+=1
-driver.press_keycode(29)
+# driver.find_element_by_id('com.izp.f2c:id/login_inputpassword').click()
+# i=1
+# while i<=6:
+#     driver.press_keycode(8)
+#     i+=1
+# driver.press_keycode(29)
 # input password '111111a'
-driver.find_element_by_id('com.izp.f2c:id/login_login_button').click()
-time.sleep(10)
+# driver.find_element_by_id('com.izp.f2c:id/login_login_button').click()
+# time.sleep(10)
 driver.find_element_by_id('com.izp.f2c:id/home_rb_home').click()
-driver.find_element_by_id('com.izp.f2c:id/top_tv_left').click()
 time.sleep(10)
-positions = []
-positions.append((174, 182))
-positions.append((264, 172))
-driver.tap(positions)
-driver.find_element_by_id('com.izp.f2c:id/product_list_tv_name').click()
-driver.find_element_by_id('com.izp.f2c:id/product_detail_bt_buy').click()
-driver.find_element_by_id('com.izp.f2c:id/order_submitbtn').click()
+driver.find_element_by_id('com.izp.f2c:id/top_tv_left').click()
+time.sleep(20)
+driver.find_element_by_android_uiautomator('new UiSelector().text("流行饰品")').click()
+
 driver.reset()
